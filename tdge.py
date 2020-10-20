f5988d8b497137d26d0f04c067cbcd18b295037e
Bekhruz Niyazov <50236015+BekhruzSNiyazov@users.noreply.github.com>
2020-10-20 12:50:03 +0300

    Update tdge.py
    This is still a pre-beta version.

--- tdge.py
+++ tdge.py
@@ -16,24 +16,52 @@
 
 # importing needed modules
 import pygame
+import math
+from datetime import datetime
 
 # the main Game class
 class Game(object):
 
 	# creating the Game window
-	def __init__(self, movement=False, width=1000, height=1000, caption="My Game", icon_path=""):
+	def __init__(self, movement=False, width=10, height=10, title="My Game", icon_path="", rotation=(0, 0, 0)):
+		
+		# checking if the user passed correct arguments; if not: raise an error
+		if type(movement) != bool:
+			raise TypeError("Movement should be a bool. If movement is False the player will not be able to move; otherwise the player will be able to move.")
+
+		if type(width) != int and type(width) != float:
+			raise TypeError("Width should be an integer or a float. [Size of width of window] = (width*100) pixels.")
+
+		if type(height) != int and type(height) != float:
+			raise TypeError("Height should be an integer or a float. [Size of height of window] = (height*100) pixels.")
+
+		if type(title) != str:
+			raise TypeError("Title should be a string. Title is the title of window.")
+
+		if type(icon_path) != str:
+			raise TypeError("Icon path should be a string. To set the icon to the window you need to provide its path.")
+
+		if type(rotation) != tuple:
+			raise TypeError("Rotation should be a touple. The first number in touple corresponds to rotation in X direction; second corresponds to rotation direction in Y direction and third corresponds to rotation in Z direction.")
+
+		if icon_path:
+			try:
+				open(icon_path, "r").close()
+			except:
+				raise FileNotFoundError("The given icon path is incorrect.")
 
 		# initializing the pygame
 		pygame.init()
 
 		# making height, width, win and movement global variables to access them in other methods
-		self.height = height
-		self.width = width
-		self.win = pygame.display.set_mode((width, height))
+		self.height = height * 100
+		self.width = width * 100
+		self.win = pygame.display.set_mode((width * 100, height * 100))
 		self.movement = movement
+		self.rotation = (0, 0, 0)
 
 		# set the title of the game ("My Game" is the default)
-		pygame.display.set_caption(caption)
+		pygame.display.set_caption(title)
 
 		# if the icon path is given: change the icon to the custom one
 		if icon_path: pygame.display.set_icon(pygame.image.load(icon_path))
@@ -68,10 +96,10 @@ class display(object):
 	# drawing the objects on the screen
 
 	# drawing a cube
-	def draw_cube(self, xyz=(1, 1, 1), coords=(0, 0), color=(100, 100, 100)):
+	def draw_cube(self, size=(1, 1, 1), coords=(0, 0, 0), color=(100, 100, 100)):
 
-		# draw a rectangle
-		pygame.draw.rect(self.win, color, ((coords[0], coords[1]), (xyz[0]*100, xyz[1]*100)))
+		# drawing a 2D rectangle
+		pygame.draw.rect(self.win, color, ((coords[0]*100, coords[1]*100), (size[0]*100, size[1]*100)))
 
 		# update the screen so that user will see the difference
 		pygame.display.update()
@@ -79,11 +107,22 @@ class display(object):
 # function, that runs the game
 def start_game(game):
 
+	# preparing to count FPS
+	start = datetime.now().second
+
 	# I use bools instead of just breaking, because 
 	# I want the while loop to end first, and then to end the game
 	running = True
 
+	frame_count = 0
 	while running:
+
+		# printing FPS every second
+		now = datetime.now().second
+		if now - start >= 1:
+			print(frame_count)
+			start = datetime.now().second
+			frame_count = 0
 
 		# if clicked the "x" button: quit the game
 		if pygame.event.get(pygame.QUIT): running = False
@@ -97,6 +136,9 @@ def start_game(game):
 		# if the movement is enabled: move the user when he presses WASD keys and rotate if he is moving his mouse
 		if game.movement:
 			pass
+
+		# increasing FPS because the while loop ended and will start again
+		frame_count += 1
 
 	# exit the game when it is closed
 	exit()
