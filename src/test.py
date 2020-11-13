01b691ef0b4044b918c3c66ac8a807908739188f
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2020-11-13 18:25:31 +0200

    used the latest features of the module

--- src/test.py
+++ src/test.py
@@ -1,13 +1,9 @@
 from tdge import *
 
-game = Game(movement=True, width=500, height=500, title="Hello, test", resizable=True)
+game = Game(movement=True, width=500, height=500, title="Hello, test", resizable=True, velocity=0.1)
 
-cube = Cube([100, 100, 100], color=[255, 255, 255], coords=[200, 200, 100])
+cube = Cube([100, 100, 100], color=[255, 255, 255], position=[200, 200, 100])
 
 display.draw(game, cube)
 
-def code():
-    game.win.fill((0, 255, 255))
-    pygame.display.update()
-
-start_game(game, pygame_code=code)
+start_game(game)
