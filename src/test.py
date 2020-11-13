e5765b629d19205cb388d62257baa2a218cb7904
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2020-11-13 18:34:08 +0200

    made the code a tiny bit more readable

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ from tdge import *
 
 game = Game(movement=True, width=500, height=500, title="Hello, test", resizable=True, velocity=0.1)
 
-cube = Cube([100, 100, 100], color=[255, 255, 255], position=[200, 200, 100])
+cube = Cube(size=[100, 100, 100], color=[255, 255, 255], position=[200, 200, 100])
 
 display.draw(game, cube)
 
