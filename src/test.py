0e3c69a49f61cfe42bd4c1a1d2613f1fae1ae460
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2020-11-14 17:39:43 +0200

    Updated test.py

--- src/test.py
+++ src/test.py
@@ -2,9 +2,7 @@ from tdge import *
 
 game = Game(movement=True, width=500, height=500, title="Hello, test", resizable=True, velocity=0.1)
 
-display.set_background(game, background_type="color", color=[255, 0, 0])
-
-cube = Cube(size=[100, 100, 100], color=[0, 255, 0], position=[200, 200, 100])
+cube = Cube(size=[100, 100, 100], color=[255, 255, 255], position=[200, 200, 0])
 
 display.draw(game, cube)
 
