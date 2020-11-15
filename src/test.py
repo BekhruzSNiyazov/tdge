4d04104e612a5127c46997da1f4fa8488eafb90f
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2020-11-15 18:26:50 +0200

    updated test.py

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ from tdge import *
 
 game = Game(movement=True, width=500, height=500, title="Rotating cube", resizable=True, velocity=1)
 
-cube = Cube(size=[100, 100, 100], color=[255, 255, 255], position=[100, 200, 0])
+cube = Cube(size=[100, 100, 100], color=[255, 255, 255], position=[200, 200, 0])
 
 display.draw(game, cube)
 
