c6dbddf38a7d072f81ebe29d64d78817eb08fb11
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2020-11-21 15:56:09 +0200

    Updated tdge.py

--- src/test.py
+++ src/test.py
@@ -2,8 +2,10 @@ from tdge import *
 
 game = Game(movement=True, width=500, height=500, title="3D Game Engine Test", velocity=1)
 
-cube = Cube(size=[100, 100, 100], color=[255, 255, 255], position=[0, 0, -51], rotation=[0, 45, 0])
+cube = Cube(size=[100, 100, 100], color=[255, 255, 255], position=[0, 0, -51], rotation=[0, 0, 0])
 
 display.draw(game, cube)
+
+display.set_background(game, background_type="color", color=[0, 255, 0])
 
 start_game(game)
