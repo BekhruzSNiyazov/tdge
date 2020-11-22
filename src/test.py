739022de1868832e9354c5c02ef10e037572dbf3
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2020-11-22 14:39:02 +0200

    fullscreen game

--- src/test.py
+++ src/test.py
@@ -1,8 +1,8 @@
 from tdge import *
 
-game = Game(movement=True, width=500, height=500, title="3D Game Engine Test", velocity=1)
+game = Game(movement=True, width=1920, height=1080, title="3D Game Engine Test", velocity=1, fullscreen=True)
 
-cube = Cube(size=[100, 100, 100], color=[255, 255, 255], position=[0, 0, -51], rotation=[0, 0, 0])
+cube = Cube(size=[100, 100, 100], color=[255, 255, 255], position=[0, 0, 100], rotation=[0, 0, 0])
 
 display.draw(game, cube)
 
